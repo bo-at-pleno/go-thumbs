3174feb0b284a7cdbb1c9f2d517ca5b3c59ad4d7
Bo Lu <bo.lu@utexas.edu>
2023-07-23 14:43:25 -0700

    wip

--- go.mod
+++ go.mod
@@ -59,10 +59,11 @@ require (
 	github.com/urfave/cli/v2 v2.25.7 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/crypto v0.10.0 // indirect
+	golang.org/x/image v0.9.0 // indirect
 	golang.org/x/mod v0.11.0 // indirect
 	golang.org/x/net v0.11.0 // indirect
 	golang.org/x/sys v0.9.0 // indirect
-	golang.org/x/text v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
 	golang.org/x/tools v0.10.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
--- internal/helpers/image.go
+++ internal/helpers/image.go
@@ -2,11 +2,115 @@ package helpers
 
 import (
 	"image"
+	"image/color"
 	"image/jpeg"
 	"os"
+
+	"golang.org/x/image/draw"
 )
 
-func readTiffAndCreateThumbnail(tiffPath string) (*image.Image, error) {
+// ThumbnailOptions holds the configuration for generating a thumbnail.
+type ThumbnailOptions struct {
+	Interpolation                    InterpolationMethod
+	Width, Height                    int
+	LowerPercentile, UpperPercentile float64
+}
+
+// InterpolationMethod specifies the type of interpolation to use for resizing.
+type InterpolationMethod int
+
+const (
+	// NearestNeighbor interpolation method.
+	NearestNeighbor InterpolationMethod = iota
+	// Bilinear interpolation method.
+	Bilinear
+	// Bicubic interpolation method.
+	Bicubic
+)
+
+// Thumbnail generates a thumbnail of the input image based on the provided options.
+func Thumbnail(img image.Image, options ThumbnailOptions) image.Image {
+	// Step 1: Resize the image using the specified interpolation method.
+	resizedImg := resizeImage(img, options.Width, options.Height, options.Interpolation)
+
+	// Step 2: Autocontrast the image based on the specified clipping percentiles.
+	autoContrastedImg := autoContrast(resizedImg, options.LowerPercentile, options.UpperPercentile)
+
+	return autoContrastedImg
+}
+
+// resizeImage resizes the input image using the specified interpolation method.
+func resizeImage(img image.Image, width, height int, interpolation InterpolationMethod) image.Image {
+	// Create a new destination image with the specified width and height.
+	dst := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	if interpolation == NearestNeighbor {
+		draw.NearestNeighbor.Scale(dst, dst.Bounds(), img, img.Bounds(), draw.Over, nil)
+	} else if interpolation == Bilinear {
+		draw.BiLinear.Scale(dst, dst.Bounds(), img, img.Bounds(), draw.Over, nil)
+	} else {
+		draw.CatmullRom.Scale(dst, dst.Bounds(), img, img.Bounds(), draw.Over, nil)
+	}
+	return dst
+}
+
+// autoContrast applies autocontrast to the input image based on the specified clipping percentiles.
+func autoContrast(img image.Image, lowerPercentile, upperPercentile float64) image.Image {
+	// Calculate the histogram of the image.
+	histogram := make(map[uint8]int)
+	totalPixels := img.Bounds().Dx() * img.Bounds().Dy()
+
+	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+			pixel := color.GrayModel.Convert(img.At(x, y)).(color.Gray)
+			histogram[pixel.Y]++
+		}
+	}
+
+	// Calculate the cumulative distribution function (CDF).
+	var cdf [256]int
+	sum := 0
+	for i := 0; i < 256; i++ {
+		sum += histogram[uint8(i)]
+		cdf[i] = sum
+	}
+
+	// Calculate the lower and upper thresholds based on percentiles.
+	lowerThreshold := int(lowerPercentile / 100 * float64(totalPixels))
+	upperThreshold := int(upperPercentile / 100 * float64(totalPixels))
+
+	// Calculate the intensity range based on the thresholds.
+	var minIntensity, maxIntensity uint8
+	for i := 0; i < 256; i++ {
+		if cdf[i] >= lowerThreshold {
+			minIntensity = uint8(i)
+			break
+		}
+	}
+
+	for i := 255; i >= 0; i-- {
+		if cdf[i] <= upperThreshold {
+			maxIntensity = uint8(i)
+			break
+		}
+	}
+
+	// Perform the linear contrast stretch.
+	bounds := img.Bounds()
+	stretchedImg := image.NewGray(bounds)
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			pixel := color.GrayModel.Convert(img.At(x, y)).(color.Gray)
+			intensity := pixel.Y
+			newIntensity := uint8(float64(intensity-minIntensity) / float64(maxIntensity-minIntensity) * 255)
+			stretchedImg.SetGray(x, y, color.Gray{Y: newIntensity})
+		}
+	}
+
+	return stretchedImg
+}
+
+func ReadTiff(tiffPath string) (*image.Image, error) {
 	// Read the TIFF file.
 	tiffFile, err := os.Open(tiffPath)
 	if err != nil {
@@ -14,22 +118,16 @@ func readTiffAndCreateThumbnail(tiffPath string) (*image.Image, error) {
 	}
 
 	defer tiffFile.Close()
-
 	tiffImage, _, err := image.Decode(tiffFile)
+
 	if err != nil {
 		return nil, err
 	}
 
-	// Create a thumbnail from the TIFF image.
-	thumbnail, err := thumbnail.Thumbnail(tiffImage, 100, 100, image.Lanczos3)
-	if err != nil {
-		return nil, err
-	}
-
-	return thumbnail, nil
+	return &tiffImage, nil
 }
 
-func saveThumbnail(thumbnail *image.Image, path string) error {
+func SaveThumbnail(thumbnail *image.Image, path string) error {
 	// Save the thumbnail to a file.
 	thumbnailFile, err := os.Create(path)
 	if err != nil {
